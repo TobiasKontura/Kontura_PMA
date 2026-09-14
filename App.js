eb0de04fae3c210539d26f26c31afb9eb21eba42
TobiasKontura <mkontura@gmail.com>
2026-09-14 12:09:24 +0200

    Initialize repository

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#d35b5b",
+    backgroundColor: "#ffffff",
     alignItems: "center",
     justifyContent: "center",
   },
